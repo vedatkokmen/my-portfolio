bcce7537b9c58beac03b8e6d8158fd2916401943
vedatkokmen <vedatkoekmen@gmail.com>
2021-03-19 03:21:27 +0300

    second commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { SocialIcon } from "react-social-icons";
 
 const NavBar = () => {
   return (
@@ -15,6 +14,7 @@ const NavBar = () => {
           >
             Vedat
           </NavLink>
+
           <NavLink
             to="/post"
             activeClassName="text-red-100 bg-red-700"
@@ -37,29 +37,6 @@ const NavBar = () => {
             About Me
           </NavLink>
         </nav>
-        <div className="inline-flex py-3 px-3 my-6">
-          <SocialIcon
-            url="https://instagram.com/vedatkokmen"
-            className="mr-4"
-            target="_blank"
-            fgColor="#fff"
-            style={{ height: 35, width: 35 }}
-          />
-          <SocialIcon
-            url="https://linkedin.com/in/vedatkokmen"
-            className="mr-4"
-            target="_blank"
-            fgColor="#fff"
-            style={{ height: 35, width: 35 }}
-          />
-          <SocialIcon
-            url="https://facebook.com/kokmenvedat"
-            className="mr-4"
-            target="_blank"
-            fgColor="#fff"
-            style={{ height: 35, width: 35 }}
-          />
-        </div>
       </div>
     </header>
   );
